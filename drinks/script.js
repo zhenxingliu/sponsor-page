ecceb17a8b4e8079df4b9c7ce6cc8b8923315a3e
liuzhenxing <5210167@qq.com>
2023-05-18 09:43:17 +0200

    update QR

--- drinks/script.js
+++ drinks/script.js
@@ -11,7 +11,7 @@ var drinks_qrcodes = $('#drinks-qrcodes');
 var drinks_qrcode = $('#drinks-qrcode');
 var isMobile = /Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent);
 
-var GithubLink  =   "https://github.com/Kaiyuan/donate-page/";
+var GithubLink  =   "https://github.com/zhenxingliu/sponsor-page/";
 var PayPalLink  =   "https://www.paypal.me/KaiyuanXie";
 $('#paypal-donate>a').href = PayPalLink;
 $('#github-box>a').href = GithubLink;
